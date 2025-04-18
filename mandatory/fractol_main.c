00f3dcba630e3956b0e97b3b48aeed234e0c05fa
Cats_have7lives <165317902+CatsHave7lives@users.noreply.github.com>
2025-04-18 21:04:42 +0100

    50%

--- mandatory/fractol_main.c
+++ mandatory/fractol_main.c
@@ -6,25 +6,26 @@
 /*   By: aessaber <aessaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:05:32 by aessaber          #+#    #+#             */
-/*   Updated: 2025/04/15 17:32:30 by aessaber         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:03:54 by aessaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol_header.h>
 
 // main
+
 static int	set_is_correct(char *str_av, char *str_set)
 {
 	int	col;
 
 	col = 0;
-	while (str_av[col])
+	while (str_av[col] && str_set)
 	{
 		if (str_av[col] != str_set[col])
 			return (FALSE);
 		col++;
 	}
-	if (str_set[col] == '\0')
+	if (str_set[col] != '\0')
 		return (FALSE);
 	return (TRUE);
 }
